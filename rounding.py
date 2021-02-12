93c9d40afa3ea0fc63eda6db9b7c0da33abb0b6b
Aurelie Ferron <aurelieferron@Aurelies-MacBook-Pro.local>
2021-02-12 11:54:48 +0100

    second sql

---
+++ rounding.py
@@ -0,0 +1,4 @@
+# From the code wars challenge : https://www.codewars.com/kata/594a6133704e4daf5d00003d/ 
+
+SELECT FLOOR(number1) AS number1, CEILING(number2) AS number2
+FROM decimals;
--- simple_join.py
+++ simple_join.py
@@ -1,2 +1,6 @@
 SELECT products.id, products.name, isbn, price, companies.id AS company_id, companies.name AS company_name FROM products LEFT JOIN companies WHERE products.company_id == companies.id
 
+# from the code wars challenge: 
+# https://www.codewars.com/kata/5802e32dd8c944e562000020/solutions/sql/me/best_practice
+
+
